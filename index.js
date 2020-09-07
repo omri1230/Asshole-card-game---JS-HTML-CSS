4f8b156e9d0634f951d3c8581105793bbd5a3170
omri1230 <omria60@gmail.com>
2020-09-07 11:10:51 +0300

    add fundamental

---
+++ index.js
@@ -0,0 +1,18 @@
+const deck = {
+    2: "2",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+    9: "9",
+    10: "10",
+    11: "Jack",
+    12: "Queen",
+    13: "King",
+    14:"Ace"
+}
+
+const sutis = ['clubs', 'diamonds', 'hearts', 'spades'];
+
